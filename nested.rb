476172d6d224b29d8ebaf5c5a3ac7715f8cb3616
Luis Penaloza <ldp231@yahoo.com>
2020-09-17 19:30:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -119,10 +119,9 @@ def changing_alan
     
 programmer_hash[:alan_kay][:known_for] = "GUI"
 
-
-
 end
 puts programmer_hash
+
 def adding_to_dennis
   # add "Assembly" to Dennis Ritchie's languages array
   # return the entire updated hash
